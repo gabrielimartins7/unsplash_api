60d55dda7233a2d6318e3bd05835e1b69a15ae40
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2023-02-23 15:49:39 -0300

    Criando rota de delete e put

--- src/index.js
+++ src/index.js
@@ -22,4 +22,21 @@ app.post("/image", (request, response) => {
     return response.status(201).json(images);
 });
 
+app.delete("/:index", (request, response) => {
+    const { index } = request.params
+
+    images.splice(index, 1);
+
+    return response.status(200).json(images);
+});
+
+app.put("/:index", (request, response) => {
+    const { index } = request.params
+    const { text } = request.body
+
+    images[index].title = text;
+    
+    return response.status(200).json(images);
+});
+
 app.listen(3333);
